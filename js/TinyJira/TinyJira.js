750f35fdf63ad60b534647ddbc4528c5b4783736
Sergey veged Berezhnoy <veged@mail.ru>
2008-11-09 05:45:23 +0300

    Добавлена функция начальной инициализации с удалённым soap интерфейсом.

--- js/TinyJira/TinyJira.js
+++ js/TinyJira/TinyJira.js
@@ -1,1 +1,27 @@
-var TinyJira = {};
+var TinyJira = {
+    jira: {
+        url: location.protocol + '//' + location.host + '/'
+    },
+    init: function(callback) {
+        var thisTinyJira = this;
+        jQuery.jsonRpc({
+            url: thisTinyJira.jira.url + 'plugins/servlet/rpc/json',
+            method: 'jira.getSoapService',
+            params: ['jirasoapservice-v2-yandex'],
+            success: function(x){
+                thisTinyJira.jira.soap = '.obj[' + x.result.objectID + ']';
+                callback.call(thisTinyJira);
+            }
+        });
+        jQuery.jsonRpc({
+            url: this.jira.url + '/plugins/servlet/rpc/json',
+            method:'jira.getCurrentUser',
+            params:[null],
+            success:function(x){
+                if (!x.result) {
+                    alert('Вы не авторизованы.');
+                }
+            }
+        });
+    }
+};
