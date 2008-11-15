49df0a19c310f598bbaa4071a4019b1edcdedf3a
Sergey veged Berezhnoy <veged@mail.ru>
2008-11-16 01:29:37 +0300

    Псевдоссылки в фильтрах.

--- js/TinyJira/Filter.js
+++ js/TinyJira/Filter.js
@@ -7,34 +7,36 @@ TinyJira.Filter = function(json) {
 
 TinyJira.Filter.prototype.toDOM = function(parentNode) {
     var thisFilter = this;
-    var li = $(document.createElement('li'))
-        .append(
-            $('<a' +
-                ' href="' + TinyJira.jira.url + 'secure/IssueNavigator.jspa?requestId=' + thisFilter.json.id + '&mode=hide">' +
-                    thisFilter.json.name +
-            '</a>')
-            .click(function(e){
-                e.preventDefault();
-                jQuery.jsonRpc({
-                    url: TinyJira.jira.url + '/plugins/servlet/rpc/json',
-                    method: TinyJira.jira.soap + '.getIssuesFromFilter',
-                    params: [null, thisFilter.json.id],
-                    success: function(x){
-                        /*
-                        $('.b-page-content').append(
-                            $('<p></p>')
-                            .text(JSON.stringify(x))
-                        );
-                        */
-                        (new TinyJira.Issues(x.result)).toDOM($('.b-page-content'));
-                    }
-                });
-            })
-        );
-        
-        if (thisFilter.json.description && thisFilter.json.description !== '') {
-            li.append($('<span>: ' + thisFilter.json.description + '</span>'));
-        }
+    var liHTML = $.htmlString('li', [
+        ['a',
+            {
+                href: TinyJira.jira.url + 'secure/IssueNavigator.jspa?requestId=' + thisFilter.json.id + '&mode=hide',
+                'class': 'b-pseudo-link'
+            },
+            ['span', thisFilter.json.name]
+        ],
+        ((thisFilter.json.description && thisFilter.json.description !== '') ? ['', ': ' + thisFilter.json.description] : [])
+    ]);
+
+    var li = $(liHTML);
+
+    li.find('a').click(function(e){
+        jQuery.jsonRpc({
+            url: TinyJira.jira.url + '/plugins/servlet/rpc/json',
+            method: TinyJira.jira.soap + '.getIssuesFromFilter',
+            params: [null, thisFilter.json.id],
+            success: function(x){
+                /*
+                $('.b-page-content').append(
+                    $('<p></p>')
+                    .text(JSON.stringify(x))
+                );
+                */
+                (new TinyJira.Issues(x.result)).toDOM($('.b-page-content'));
+            }
+        });
+        return false;
+    });
 
     if (parentNode) $(parentNode).append(li);
 
