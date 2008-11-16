b900c6eb10636a5cdbba90c3a6cfb7848376ccfb
Sergey veged Berezhnoy <veged@mail.ru>
2008-11-16 17:22:34 +0300

    Перезагрузка и скрытие списка issue.

--- js/TinyJira/Issues.js
+++ js/TinyJira/Issues.js
@@ -8,7 +8,18 @@ TinyJira.Issues = function(json) {
 TinyJira.Issues.prototype.toDOM = function(parentNode) {
     var thisIssues = this,
         html = $.htmlString('div', [
-            ['h2', 'Задачи'],
+            ['h2', [
+                [null, 'Задачи'],
+                (thisIssues.json ?
+                    ['span', {'class':'b-sup-controls'}, [
+                        [null, ' &nbsp;'],
+                        ['sup', ['a', {'class':'b-pseudo-link refresh'}, ['span', 'обновить']]],
+                        [null, ' &nbsp;'],
+                        ['sup', ['a', {'class':'b-pseudo-link hide'}, ['span', 'скрыть']]]
+                    ]] :
+                    []
+                )
+            ]],
             ['div', {'class': 'b-issues-table TinyJira-c-IssuesTable'},
                 (thisIssues.json ?
                     ['table', ['tr', [
@@ -31,6 +42,23 @@ TinyJira.Issues.prototype.toDOM = function(parentNode) {
         $(thisIssues.json.splice(0, 50)).each(function(){
             (new TinyJira.Issue(this)).toDOM(table);
         });
+
+        dom.find('.b-sup-controls .refresh').click(function(e){
+            e.preventDefault();
+            delete thisIssues.json;
+            var oldDOM = thisIssues.dom;
+            var newDOM = thisIssues.toDOM();
+            oldDOM.hide().after(newDOM);
+            setTimeout(function(){oldDOM.remove()}, 1);
+        });
+
+        dom.find('.b-sup-controls .hide').click(function(e){
+            e.preventDefault();
+            thisIssues.dom.hide();
+            delete thisIssues.json;
+            setTimeout(function(){thisIssues.dom.remove()}, 1);
+        });
+
     } else {
         jQuery.jsonRpc({
             url: TinyJira.jira.url + '/plugins/servlet/rpc/json',
@@ -47,6 +75,6 @@ TinyJira.Issues.prototype.toDOM = function(parentNode) {
     }
 
     if (parentNode) $(parentNode).append(dom);
-    
+
     return dom;
 };
