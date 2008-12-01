23779a902dd5d73af144c1bbce18034f96761b5c
Sergey veged Berezhnoy <veged@mail.ru>
2008-12-01 10:38:52 +0300

    Всплывающие подсказки для лейблов в описании issue.

--- js/TinyJira/Issue.js
+++ js/TinyJira/Issue.js
@@ -167,14 +167,14 @@ TinyJira.Issue.prototype.toDOM = function(parentNode) {
                 (function(){
                     var result = $.htmlString('a', {'class': 'summary', href: 'javascript:'}, '&ensp;' + $.htmlStringText(thisIssue.json.summary) + '&ensp;');
 
-                    result += ' ' + $.htmlString('span', {'class':'assignee'}, [
+                    result += ' ' + $.htmlString('span', {'class':'assignee', title: 'Исполнитель'}, [
                             [null, '&nbsp;&#9786;'],
                             ['a', {'class':'b-pseudo-link assignee-change', href: 'javascript:'}, ['span', thisIssue.json.assignee.login || thisIssue.json.assignee]],
                             [null, '&ensp;']
                         ]);
 
                     if (thisIssue.json.components && thisIssue.json.components.length > 0) {
-                        result += ' ' + $.htmlString('span', {'class':'components'}, [
+                        result += ' ' + $.htmlString('span', {'class':'components', title: 'Компоненты'}, [
                                 [null, '&nbsp;&there4;'],
                                 ['a', {'class':'b-pseudo-link components-change', href: 'javascript:'}, ['span',
                                     $.map(thisIssue.json.components, function(v){ return v.name }).join(', ')
@@ -184,7 +184,7 @@ TinyJira.Issue.prototype.toDOM = function(parentNode) {
                     }
 
                     if (thisIssue.json.fixVersions && thisIssue.json.fixVersions.length > 0) {
-                        result += ' ' + $.htmlString('span', {'class':'fixversions'}, [
+                        result += ' ' + $.htmlString('span', {'class':'fixversions', title: 'Версии'}, [
                                 [null, '&ensp;&beta; '],
                                 ['a', {'class':'b-pseudo-link fixversions-change', href: 'javascript:'}, ['span',
                                     $.map(thisIssue.json.fixVersions, function(v){ return v.name }).join(', ')
